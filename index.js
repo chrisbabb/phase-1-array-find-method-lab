6a41436b281227d197e5e3c150411ac0afb028ce
Christopher Babb <chris@appsoftdev.com>
2022-06-08 17:26:33 -0700

    commit tree 50c2892b2c5a96fd3c32182a2f6849a1efb23e48

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(result) {
+    const foundYear = result.find(recordWL => recordWL.result === "W")
+    if(typeof foundYear !== 'undefined'){
+        return foundYear.year
+    }
+    return foundYear
+}
